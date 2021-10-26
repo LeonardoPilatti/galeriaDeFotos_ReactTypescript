22706e77c0da726264bcd12ad12f7c8306515e63
Leonardo Pilatti <leonardopilatti14@gmail.com>
2021-10-26 16:50:02 -0300

    criando o layout inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as C from './App.styles';
+import * as Photos from './services/photos';
+import { Photo } from './types/Photo';
 
-function App() {
+
+const App = () => {
+  const [loading, setLoading] = React.useState(false);
+  const [photos, setPhotos] = React.useState<Photo[]>([]);
+
+  React.useEffect(() => {
+    const getPhotos = async () => {
+      setLoading(true);
+
+      setPhotos(await Photos.getAll());
+
+      setLoading(false);
+    }
+    getPhotos();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>Galeria de Fotos</C.Header>
+
+        {loading && 
+        <C.ScreenWarning>
+          <div className="emoji">
+            
+          </div>
+          <div>Carregando...</div>  
+        </C.ScreenWarning>}
+      </C.Area>
+    </C.Container>
   );
 }
 
